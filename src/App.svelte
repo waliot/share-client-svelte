efeef0808a4403381d6e24bb8beb66e5ee92f6be
Sam Bulatov <mephistorine@gmail.com>
2022-02-13 23:03:18 +0300

    feat: Карточка объекта

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import { MAP_CENTER } from "@waliot-oss/share-client-sdk"
   import { onMount } from "svelte"
   import ZoomControls from "./ZoomControls.svelte"
+  import Card from "./components/Card.svelte"
 
   /** @type {HTMLElement} */
   let mapElementRef
@@ -41,12 +42,31 @@
   }
 </script>
 
-<main class="app">
-  <ZoomControls on:zoomChange={e => onZoomChange(e)} />
-  <div id="map-container" bind:this={mapElementRef}></div>
-</main>
+<div class="card-container">
+  <Card>
+    <svelte:fragment slot="header">
+      <p class="card-title"><strong>Сэм 🛰👾💀</strong></p>
+      <p class="card-description">потеря связи 4 д. 8 ч.</p>
+    </svelte:fragment>
+
+    <ul class="card-content">
+      <li class="card-content-item">
+        <span class="item-property">Модель</span>
+        <span class="item-value">АДМ 50 - Трекер 1</span>
+      </li>
+    </ul>
+
+    <p class="tracking-object-address">Первая Мониторинговая Компания, ГЛОНАСС, улица Силантьева, 80/1, Краснодар, городской округ Краснодар, Краснодарский край</p>
+  </Card>
+</div>
+<ZoomControls on:zoomChange={e => onZoomChange(e)} />
+<div id="map-container" bind:this={mapElementRef}></div>
 
 <style>
+  :global(body) {
+    font-size: 14px;
+  }
+
   #map-container {
     position: absolute;
     top: 0;
@@ -54,5 +74,44 @@
     width: 100%;
     height: 100%;
     z-index: 1;
+  }
+
+  .card-container {
+    position: absolute;
+    top: 0;
+    right: 0;
+    z-index: 2;
+    margin-right: 20px;
+    margin-top: 14px;
+  }
+
+  .card-container :global(.card) {
+    width: 250px;
+  }
+
+  .card-content {
+    width: 250px;
+    margin: 0;
+    padding: 0;
+  }
+
+  .card-content-item {
+    padding: 6px 20px;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid hsl(199deg 18% 33%);
+  }
+
+  span.item-property {
+    color: hsl(0deg 0% 100% / 70%);
+  }
+
+  .tracking-object-address {
+    padding: 6px 20px;
+    font-size: 12px;
+    line-height: 14px;
+    color: hsl(207deg 90% 54%);
+    text-align: left;
+    margin: 0;
   }
 </style>
---
+++ src/components/Card.svelte
@@ -0,0 +1,36 @@
+<article class="card">
+  <header class="card-header">
+    <slot name="header"></slot>
+  </header>
+
+  <div class="card-content">
+    <slot></slot>
+  </div>
+</article>
+
+<style>
+  .card {
+    border-radius: 7px;
+    overflow: hidden;
+  }
+
+  .card-header {
+    background-color: var(--wlt-primary-color);
+    color: white;
+    padding: 14px 20px;
+  }
+
+  .card-header :global(.card-title),
+  .card-header :global(.card-description) {
+    margin: 0;
+  }
+
+  .card-header :global(.card-description) {
+    font-size: 12px;
+  }
+
+  .card-content {
+    background-color: var(--wlt-background-color);
+    color: white;
+  }
+</style>
